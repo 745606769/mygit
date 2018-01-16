7ec537e0548585861c99e248cb3e72dfe15d6c52
745606769 <745606769@qq.com>
2018-01-16 19:32:58 +0800

    new test

--- src/mygit/HelloWorld.java
+++ src/mygit/HelloWorld.java
@@ -2,6 +2,6 @@ package mygit;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-        System.out.println("Hello World change");
+        System.out.println("Hello World change new");
     }
 }
